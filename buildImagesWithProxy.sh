7e0498e91c5b96f482e7857fda09bf3bf3ecb491
Shen-Chris <36977943+Shen-Chris@users.noreply.github.com>
2025-07-23 18:01:48 +0800

    Update buildImagesWithProxy.sh

--- buildImagesWithProxy.sh
+++ buildImagesWithProxy.sh
@@ -1,1 +1,5 @@
+# 先把微信linux x86安装包下载到本地，和Dockerfile同级目录
+weget https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb -O weixin.deb
+
+# 构建
 docker build --no-cache -t dsq-docker-wechat:latest --build-arg HTTP_PROXY="http://192.168.1.254:7890" --build-arg HTTPS_PROXY="http://192.168.1.254:7890" --build-arg WECHAT_URL="https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb" .
