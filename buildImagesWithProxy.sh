73a6fdf52b7a1ad10692ee5e7b262add0476c6d5
Shen-Chris <36977943+Shen-Chris@users.noreply.github.com>
2025-07-23 17:54:25 +0800

    Create buildImagesWithProxy.sh

---
+++ buildImagesWithProxy.sh
@@ -0,0 +1,1 @@
+docker build --no-cache -t dsq-docker-wechat:latest --build-arg HTTP_PROXY="http://192.168.1.254:7890" --build-arg HTTPS_PROXY="http://192.168.1.254:7890" --build-arg WECHAT_URL="https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb" .
